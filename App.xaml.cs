27f526b9dacc07626b552a372b8a0265c7879d13
camilly <fcamilly129@gmail.com>
2023-07-28 22:48:24 -0300

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace note;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace note;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
